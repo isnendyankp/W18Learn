fa0f6d9a4b1e0ae4c4c4c38fa5eb0bac152c73ff
isnendyankp <isnendyankp@gmail.com>
2024-03-07 17:21:18 +0700

    Add print statements to check database connection

--- connectors/mysql_connector.py
+++ connectors/mysql_connector.py
@@ -6,9 +6,15 @@ password = ""
 host = "127.0.0.1"
 database_name = "product_review"
 
+# Print the connection string to connect to the database
+print("Connecting to SQL database")
+
 # Create the connection string
 connection_string = f'mysql+mysqlconnector://{username}:{password}@{host}/{database_name}'
 engine = create_engine(connection_string)
 
 # Connect to the database
 connection = engine.connect()
+
+# Print the connection object to check if it's connected to the database
+print("Success Connecting to SQL database")
