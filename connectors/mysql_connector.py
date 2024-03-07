471f7e54de9de066fee5961673eae6b36b5a81ee
isnendyankp <isnendyankp@gmail.com>
2024-03-07 17:18:26 +0700

    Create the connection string

--- connectors/mysql_connector.py
+++ connectors/mysql_connector.py
@@ -5,3 +5,8 @@ username = "root"
 password = ""
 host = "127.0.0.1"
 database_name = "product_review"
+
+# Create the connection string
+connection_string = f'mysql+mysqlconnector://{username}:{password}@{host}/{database_name}'
+engine = create_engine(connection_string)
+
